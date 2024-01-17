90bddbb24aff1f0ed82296f5a788a4b39090ddf3
Mamdouh Ali Hamza <mamdouh.ali93@gmail.com>
2024-01-17 15:55:06 +0200

    add page to add activities

---
+++ static/script-add.js
@@ -0,0 +1,21 @@
+document.getElementById("dateField").valueAsDate = new Date();
+
+
+
+// document.getElementById('addForm').addEventListener('submit', function (event) {
+//     event.preventDefault(); // Prevent the default form submission
+
+//     // Show the notification
+//     showNotification('Activity added successfully!');
+// });
+
+// function showNotification(message) {
+//     var notification = document.getElementById('notification');
+//     notification.innerHTML = message;
+//     notification.style.display = 'block';
+
+//     // Hide the notification after 3 seconds (adjust as needed)
+//     setTimeout(function () {
+//         notification.style.display = 'none';
+//     }, 3000);
+// }
--- tracker.py
+++ tracker.py
@@ -10,6 +10,12 @@ def get_html(page_name):
     html_file.close()
     return content
 
+# add data to text file seperated by new line
+def add_data(new_data):
+    notesdb = open("data.txt", "a")
+    notesdb.write("\n")
+    notesdb.write(new_data)
+    notesdb.close()
 
 
 # The home/login page
@@ -25,4 +31,15 @@ def dashboard():
 
 
 
-   
+# page to add new activity
+@app.route("/add")
+def add():
+    html_page = get_html("add")
+    name = flask.request.args.get("name")
+    category = flask.request.args.get("category")
+    date = flask.request.args.get("date")
+    duration = flask.request.args.get("duration")
+    new_data = str(name) + "," + str(category) + "," + str(date) + "," + str(duration)
+    add_data(new_data)
+    return html_page
+  
