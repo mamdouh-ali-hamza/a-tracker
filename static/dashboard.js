2c998f71130fa1f290a5848e3ad7dfec785a6af1
Mamdouh Ali Hamza <mamdouh.ali93@gmail.com>
2024-01-18 17:43:26 +0200

    Dashboard page (frontend, backend)

---
+++ static/dashboard.js
@@ -0,0 +1,1 @@
+document.getElementById("dailyDate").valueAsDate = new Date();
--- tracker.py
+++ tracker.py
@@ -13,9 +13,17 @@ def get_html(page_name):
 # add data to text file seperated by new line
 def add_data(new_data):
     notesdb = open("data.txt", "a")
-    notesdb.write("\n")
     notesdb.write(new_data)
+    notesdb.write("\n")
     notesdb.close()
+
+# read user data
+def get_data():
+    data_file = open("data.txt")
+    content = data_file.read()
+    data_file.close()
+    data = content.split("\n")
+    return data
 
 
 # The home/login page
@@ -26,7 +34,7 @@ def home():
 
 # registeration page
 @app.route("/register")
-def dashboard():
+def register():
     return get_html("register")
 
 
@@ -40,6 +48,71 @@ def add():
     date = flask.request.args.get("date")
     duration = flask.request.args.get("duration")
     new_data = str(name) + "," + str(category) + "," + str(date) + "," + str(duration)
-    add_data(new_data)
+    if new_data!="None,None,None,None":
+        add_data(new_data)
+    new_data = "None,None,None,None" 
     return html_page
-  
+    
+    
+
+
+#
+@app.route("/dashboard")
+def dashboard():
+    html_page = get_html("dashboard")
+    data = get_data()
+
+    table_head = "<th>Name</th><th>Category</th><th>Date</th><th>Duration</th>"
+    actual_values = ""
+
+    activities_duraion = [0, 0, 0, 0, 0, 0, 0, 0]       # [Work, Study, Sport, Social, Spiritual, Creative, Chill, Other]
+
+    for row in data:
+        actual_values += "<tr>"
+        col = row.split(",")
+
+        if col[1] == 'Work':
+            activities_duraion[0] += int(col[3])
+        elif col[1] == 'Study':
+            activities_duraion[1] += int(col[3])
+        elif col[1] == 'Sport':
+            activities_duraion[2] += int(col[3])
+        elif col[1] == 'Social':
+            activities_duraion[3] += int(col[3])
+        elif col[1] == 'Spiritual':
+            activities_duraion[4] += int(col[3])
+        elif col[1] == 'Creative':
+            activities_duraion[5] += int(col[3])
+        elif col[1] == 'Chill':
+            activities_duraion[6] += int(col[3])
+        elif col[1] == 'Other':
+            activities_duraion[7] += int(col[3])
+
+        for td in col:
+            actual_values += "<td>" + td + "</td>"
+
+        actual_values += "</tr>"
+
+    # if actual_values == "":
+    #     return html_page.replace("$$NO_DATA_MESSAGE$$", "<p>No data yet<p>")     
+    # else:
+    #     return html_page.replace("$$DATA$$", actual_values).replace("$$TABLE_HEAD$$", table_head)
+        
+    # return html_page.replace("$$DATA$$", actual_values)
+        
+    # daily_date = flask.request.args.get("dailyDate")
+    
+    
+    
+    
+
+    print(activities_duraion)
+
+    height_chart = sorted(activities_duraion)[-1] * 1.2
+    html_page = html_page.replace('$$HEIGHT$$', str(height_chart))
+
+    html_page = html_page.replace('$$WORK$$', str(activities_duraion[0])).replace('$$STUDY$$', str(activities_duraion[1])).replace('$$SPORT$$', str(activities_duraion[2])).replace('$$SOCIAL$$', str(activities_duraion[3])).replace('$$SPIRITUAL$$', str(activities_duraion[4])).replace('$$CREATIVE$$', str(activities_duraion[5])).replace('$$CHILL$$', str(activities_duraion[6])).replace('$$OTHER$$', str(activities_duraion[7]))
+    
+    return html_page.replace("$$DATA$$", actual_values).replace("$$TABLE_HEAD$$", table_head)
+
+
